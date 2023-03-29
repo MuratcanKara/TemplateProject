c4e5bd1b7ebceed05741c2404fbbd4ac23f79681
Muratcan Kara <muratfivem@gmail.com>
2023-03-29 23:16:06 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var p in productManager.GetAllByUnitPrice(10,15))
+            {
+                Console.WriteLine(p.ProductName);
+            }
+        }
+    }
+}
---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Abstract
+{
+    //If a class implements this interface ,then it is a table of DB.
+    public interface IEntity
+    {
+    }
+}
